842f0399b36600358b61dabb39073e10960daf2c
_woo_s.j <sjwoo1999@korea.ac.kr>
2025-02-27 17:51:06 +0900

    🔧 Redirect / to /api-docs
    Redirect / to /api-docs

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
-const helloWorldController = require('./api/controllers/hello_world');
+const helloWorldController = require('./api/controllers/hello_world'); // 컨트롤러 경로
 const app = express();
 
 // Swagger 문서 로드
 const swaggerDocument = YAML.load('./swagger.yaml');
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// hello_world 라우트 추가
+// 기본 경로(/)에서 /api-docs로 리디렉션
+app.get('/', (req, res) => {
+  res.redirect(302, '/api-docs'); // 302 Found로 리디렉션 (임시 리디렉션)
+  // 또는 301로 영구 리디렉션 사용 가능: res.redirect(301, '/api-docs');
+});
+
+// hello_world 컨트롤러 라우트 추가
 app.get('/hello', helloWorldController.hello);
 
 // 동적 포트 설정
