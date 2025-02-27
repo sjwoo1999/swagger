432d527498529ad130db5b3027eeae99df8e9e58
_woo_s.j <sjwoo1999@korea.ac.kr>
2025-02-27 19:52:59 +0900

    ✨ New Feature: Filtering slang words
    New Feature: Filtering slang words

--- api/controllers/commentController.js
+++ api/controllers/commentController.js
@@ -2,6 +2,7 @@
 const mockComments = [
     { comment_id: 1, content: '좋은 모집공고네요!', recruitment_id: 1 },
   ];
+  const profaneWords = ['bad', 'ugly', 'mean'];
   
   const getComments = (req, res) => {
     const { recruitment_id } = req.params;
@@ -16,9 +17,16 @@ const mockComments = [
       return res.status(400).json({ message: '댓글 내용을 입력해주세요.' });
     }
   
+    // 욕설 필터링
+    const words = content.split(' ');
+    const hasProfanity = words.some(word => profaneWords.includes(word.toLowerCase()));
+    if (hasProfanity) {
+      return res.status(400).json({ error: '댓글에 부적절한 언어가 포함되었습니다.' });
+    }
+  
     const newComment = { comment_id: mockComments.length + 1, content, recruitment_id: parseInt(recruitment_id) };
     mockComments.push(newComment);
-    res.status(200).json(newComment);
+    res.status(201).json(newComment);
   };
   
   module.exports = { getComments, createComment };
